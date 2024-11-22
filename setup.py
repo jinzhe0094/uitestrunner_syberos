c81df8a0f1aeb3926bcc62b3c366ff27d700fc60
jinzhe_wang_dev <wangjinzhe@syberos.com>
2024-11-22 14:23:12 +0800

    2.7.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='uitestrunner_syberos',
-    version='2.7.2',
+    version='2.7.3',
     author='Jinzhe Wang',
     description='A ui automated testing tool for SyberOS',
     long_description=open('README.md', 'r').read(),
